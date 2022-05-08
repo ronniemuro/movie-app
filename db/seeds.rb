a11b1fa3528d7bf87afb56d20a000267caae23ea
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-05-08 19:36:09 +0200

    wednesday homework

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+actor.save
+actor = Actor.new({ first_name: "Sunny", last_name: "Delight", known_for: "Scooby Doo" })
+actor.save
+actor = Actor.new({ first_name: "James", last_name: "Mansfield", known_for: "Who" })
+actor.save
